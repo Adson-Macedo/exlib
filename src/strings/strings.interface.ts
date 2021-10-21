d2b8eb33663f5dc8797ea1ee9999d596d0942685
Adson-Macedo <agnsoft@hotmail.com>
2021-10-21 19:49:43 -0300

    Activities
      - Adding StringContainer
      - Adding StringManager
      - Adding Strings

---
+++ src/strings/strings.interface.ts
@@ -0,0 +1,12 @@
+
+export interface IStrings {
+    WORDS: object
+    MESSAGES: object
+    DESCRIPTIONS: object
+}
+
+export interface IStringDefinition {
+    en: string
+    pt?: string
+    es?: string
+}
