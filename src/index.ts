7db5bd5cbc28b7249787fc83c8a7fe00524d141f
Adson-Macedo <agnsoft@hotmail.com>
2021-10-21 20:02:17 -0300

    Updating index

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { stringManager } from './strings/string.manager'
+
+
+export * from './exceptions'
+
+export { StringManager } from './strings/string.manager'
+
+export { IStrings } from './strings/strings.interface'
+
+export { Language } from './utils/language'
+
+export { ExceptionArgs } from './utils/exception.args'
+
+export default stringManager
